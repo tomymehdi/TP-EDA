e32754290c7c4d51d300fe9e21c591e33b0e4466
Conrado Mader Blanco <cmaderblanco@gmail.com>
2011-10-12 17:54:15 -0300

    sa

---
+++ src/game/Position.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Position {
+
+	private int row, col;
+
+	public Position(int row, int col){
+		this.row=row;
+		this.col=col;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+}
