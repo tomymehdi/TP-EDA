e9a34cef1ced63f2faf9feb485dd5c07a6d3f2bc
framundo <feedeee@hotmail.com>
2011-10-12 18:11:55 -0300

    d

--- src/gui/BoardPanel.java
+++ src/gui/BoardPanel.java
@@ -3,6 +3,7 @@ package gui;
 import game.Board;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -14,5 +15,29 @@ public class BoardPanel extends JPanel {
 	public BoardPanel(Board board){
 		this.board=board;
 		setPreferredSize(new Dimension(Board.SIZE*TILE_SIZE,Board.SIZE*TILE_SIZE));
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		drawGrid(g);
+		drawTiles(g);
+	}
+	
+	private void drawGrid(Graphics g){
+		for(int col=0; col<Board.SIZE; col++){
+			g.drawLine(col*TILE_SIZE, 0, col*TILE_SIZE, Board.SIZE*TILE_SIZE);
+		}
+		for(int row=0; row<Board.SIZE; row++){
+			g.drawLine(0, row*TILE_SIZE, Board.SIZE*TILE_SIZE, row*TILE_SIZE);
+		}
+	}
+	
+	private void drawTiles(Graphics g){
+		for(int row=0; row<Board.SIZE; row++){
+			for(int col=0; col<Board.SIZE; col++){
+				if(board.getTile(row,col))
+			}
+		}
 	}
 }
