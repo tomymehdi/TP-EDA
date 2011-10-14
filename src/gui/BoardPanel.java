64dbd86a3f46923cd4b917077b9025a64a57be69
Tomas Mehdi <tomymehdi@gmail.com>
2011-10-14 15:05:35 -0300

    validation no posible moves

--- src/gui/BoardPanel.java
+++ src/gui/BoardPanel.java
@@ -14,64 +14,69 @@ import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
-	
+
 	private Board board;
-	private static final int TILE_SIZE=30;
-	
-	public BoardPanel(Board board){
-		this.board=board;
-		setPreferredSize(new Dimension(Board.SIZE*TILE_SIZE,Board.SIZE*TILE_SIZE));
+	private static final int TILE_SIZE = 30;
+
+	public BoardPanel(Board board) {
+		this.board = board;
+		setPreferredSize(new Dimension(Board.SIZE * TILE_SIZE, Board.SIZE
+				* TILE_SIZE));
 		setBackground(Color.WHITE);
-		addMouseListener(new MouseAdapter(){
+		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent m) {
-				if(BoardPanel.this.board.isPlayerTurn()){
-					int row=m.getY()/TILE_SIZE;
-					int col=m.getX()/TILE_SIZE;
-					BoardPanel.this.board=BoardPanel.this.board.putTile(row, col, Tile.PLAYER1);
-					BoardPanel.this.repaint();	
+				if (BoardPanel.this.board.isPlayerTurn()) {
+					int row = m.getY() / TILE_SIZE;
+					int col = m.getX() / TILE_SIZE;
+					BoardPanel.this.board = BoardPanel.this.board.putTile(row,
+							col, Tile.PLAYER1);
+					BoardPanel.this.repaint();
 				}
-			}			
+			}
 		});
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		drawGrid(g);
 		drawTiles(g);
 	}
-	
-	private void drawGrid(Graphics g){
+
+	private void drawGrid(Graphics g) {
 		g.setColor(Color.LIGHT_GRAY);
-		for(int col=0; col<Board.SIZE; col++){
-			g.drawLine(col*TILE_SIZE, 0, col*TILE_SIZE, Board.SIZE*TILE_SIZE);
+		for (int col = 0; col < Board.SIZE; col++) {
+			g.drawLine(col * TILE_SIZE, 0, col * TILE_SIZE, Board.SIZE
+					* TILE_SIZE);
 		}
-		for(int row=0; row<Board.SIZE; row++){
-			g.drawLine(0, row*TILE_SIZE, Board.SIZE*TILE_SIZE, row*TILE_SIZE);
+		for (int row = 0; row < Board.SIZE; row++) {
+			g.drawLine(0, row * TILE_SIZE, Board.SIZE * TILE_SIZE, row
+					* TILE_SIZE);
 		}
 	}
-	
-	private void drawTiles(Graphics g){
-		for(int row=0; row<Board.SIZE; row++){
-			for(int col=0; col<Board.SIZE; col++){
-				Tile tile=board.getTile(row,col);
-				if(tile==Tile.PLAYER1){
+
+	private void drawTiles(Graphics g) {
+		for (int row = 0; row < Board.SIZE; row++) {
+			for (int col = 0; col < Board.SIZE; col++) {
+				Tile tile = board.getTile(row, col);
+				if (tile == Tile.PLAYER1) {
 					g.setColor(Color.BLACK);
-					g.fillOval(col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE);
-				}
-				else if(tile==Tile.PLAYER2){
+					g.fillOval(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE,
+							TILE_SIZE);
+				} else if (tile == Tile.PLAYER2) {
 					g.setColor(Color.LIGHT_GRAY);
-					g.fillOval(col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+					g.fillOval(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE,
+							TILE_SIZE);
 				}
 			}
 		}
 	}
-	
+
 	public void setBoard(Board board) {
 		this.board = board;
 	}
-	
+
 	public Board getBoard() {
 		return board;
 	}
--- src/gui/Window.java
+++ src/gui/Window.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyEvent;
 import java.io.File;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import parser.Parser;
@@ -23,10 +24,17 @@ public class Window extends JFrame {
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				Board b=Window.this.boardPanel.getBoard();
-				if(e.getKeyCode()==KeyEvent.VK_ENTER && !b.isPlayerTurn()){
-					Window.this.boardPanel.setBoard(b.computerTurn());
-					Window.this.repaint();
+				Board b = Window.this.boardPanel.getBoard();
+				if (e.getKeyCode() == KeyEvent.VK_ENTER && !b.isPlayerTurn()) {
+					Board auxBoard = b.computerTurn();
+					if (auxBoard == null) {
+						JOptionPane.showMessageDialog(null,
+								"No posible moves", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					} else {
+						Window.this.boardPanel.setBoard(auxBoard);
+						Window.this.repaint();
+					}
 				}
 			}
 		});
@@ -38,16 +46,15 @@ public class Window extends JFrame {
 		this(new Board());
 	}
 
-
 	public static void main(String[] args) {
-		Parser p=new Parser(new File("./boards/lala.txt"));
+		Parser p = new Parser(new File("./boards/lala.txt"));
 		Board b;
-		try{
-			b=p.parseFile();
-		} catch(Exception e){
+		try {
+			b = p.parseFile();
+		} catch (Exception e) {
 			throw new RuntimeException("Invalid File");
 		}
-		b=new Board();
+		b = new Board();
 		new Window(b);
 	}
 }
